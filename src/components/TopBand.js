4852501bf57adddcda9e6b380dca4d34dc95b431
Tausif Nawaz <nawaztausif63@gmail.com>
2022-06-18 01:21:57 +0530

    Added the topband

---
+++ src/components/TopBand.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './topBand.css';
+import {FaApple} from 'react-icons/fa'
+import {FaAndroid} from 'react-icons/fa'
+import {FaFacebook} from 'react-icons/fa'
+import {FaYoutube} from 'react-icons/fa'
+import {FaInstagram} from 'react-icons/fa'
+import {FaTelegram} from 'react-icons/fa'
+
+const TopBand = () => {
+    return(
+        <div className="topBand">
+            <div className="topBand_left">
+                <p>Buy BTC: $2462</p>
+                <div className='div'></div>
+                <p>Sell BTC: $2465</p>
+            </div>
+            <div className="topBand_right">
+                <FaApple className='fa ap' />
+                <div className='div'></div>
+                <FaAndroid className='fa ad'/>
+                <div className='div'></div>
+                <FaFacebook className='fa fb'/>
+                <div className='div'></div>
+                <FaYoutube className='fa yt'/>
+                <div className='div'></div>
+                <FaInstagram className='fa ins'/>
+                <div className='div'></div>
+                <FaTelegram className='fa tg'/>
+            </div>
+        </div>
+        
+    )
+}
+
+
+export default TopBand;
