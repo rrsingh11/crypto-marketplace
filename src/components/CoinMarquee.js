608bf49840f39bc788bd464029282a9c89556988
Tausif Nawaz <nawaztausif63@gmail.com>
2022-06-24 20:51:15 +0530

    Css fixes

--- src/components/CoinMarquee.js
+++ src/components/CoinMarquee.js
@@ -4,17 +4,18 @@ import React from 'react'
 const CoinMarquee = () => {
   return (
     <div style={{marginBottom:"10px"}}>
-<div style={{height:"62px", backgroundColor: "#FFFFFF", overflow:"hidden", boxSizing: "border-box",
-  borderRadius: "4px", textAlign: "right", lineHeight:"14px", blockSize:"62px", 
-  fontSize: "12px", fontFeatureSettings: "normal", textSizeAdjust: "100%", boxShadow: "4.7px 9.4px 9.4px hsl(0deg 0% 0% / 0.35)",
-  padding:"1px", margin: "0px", width: "100%"}}>
-  <div style={{height:"40px", padding:"0px", margin:"0px", width: "100%"}}>
-    <iframe title='coinMarquee' src="https://widget.coinlib.io/widget?type=horizontal_v2&theme=light&pref_coin_id=1505&invert_hover=no" 
-      width="100%" height="36px" scrolling="auto" marginwidth="0" marginheight="0" frameborder="0" border="0" style={{border:"0",margin:"0",padding:"0"}}>
-        </iframe>
+      <div style={{height:"50px", backgroundColor: "#FFFFFF", overflow:"hidden", boxSizing: "border-box",
+        borderRadius: "4px", textAlign: "right", lineHeight:"14px", blockSize:"50px", 
+        fontSize: "12px", fontFeatureSettings: "normal", textSizeAdjust: "100%", boxShadow: "0 0 5px #808080",
+        padding:"1px", margin: "0px", width: "100%"}}>
+        <div style={{height:"40px", padding:"0px", margin:"0px", width: "100%",backgroundColor: "#FFFFFF"}}>
+          <iframe title='coinMarquee' src="https://widget.coinlib.io/widget?type=horizontal_v2&theme=light&pref_coin_id=1505&invert_hover=no" 
+            width="100%" height="36px" scrolling="auto" marginwidth="0" marginheight="0" frameborder="0" border="0" 
+            style={{border:"0",margin:"0",padding:"0",backgroundColor:'#006ee6'}}>
+          </iframe>
         </div>
-  </div>
-</div>
+      </div>
+    </div>
   )
 }
 
